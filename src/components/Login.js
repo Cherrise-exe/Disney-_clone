4ec63d8ea424c2d59bbf94812208f42cdec2b962
Cherrise Hatcher <cherrisehatcher@gmail.com>
2021-04-29 20:04:32 -0400

    add sign in button and logo bug fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,10 +5,10 @@ const Login = (props) => {
     <Container>
         <Content>
           <CTA>
-              <CTALogoOne src='/images/cta-logo-one.svg' />
+          <CTALogoOne src='/images/cta-logo-one.svg' alt=""/>  
+          <SignUp>GET ALL THERE</SignUp>
           </CTA>
-            <BgImage />
-            
+          <BgImage />
         </Content>
     </Container>
     );
@@ -49,8 +49,36 @@ const BgImage = styled.div`
     z-index: -1;
 `;
 
-const CTA = styled.div``;
+const CTA = styled.div`
+max-width: 650px;
+width: 100%;
+display: flex;
+flex-direction: column;
+`;
 
-const CTALogoOne = styled.img``;
+const CTALogoOne = styled.img`
+margin-bottom: 12px;
+max-width: 600px;
+min-height: 1px;
+display: block;
+width: 100%;
+`;
+
+const SignUp = styled.a`
+font-weight: bold;
+color: #f9f9f9;
+background-color: #0063e5;
+margind-botton: 12px;
+width: 100%;
+letter-spacing: 1.5px;
+font-size: 18px;
+padding: 16.5px 0;
+border: 1px solid transparent;
+border-radius: 4px;
+
+&:hover {
+    background-color: #0483ee;
+}
+`;
 
 export default Login;
