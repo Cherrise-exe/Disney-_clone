2323eaad5a78268c2c8c5a9d53a8477e12a10b90
Cherrise Hatcher <cherrisehatcher@gmail.com>
2022-01-26 18:34:27 -0500

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./App.css";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
-import 'dotenv/config' 
-import express from 'express';
 
 ReactDOM.render(
   <React.StrictMode>
